89cafa1e4650ce63db7d6ab31cda708acbbe86b1
NotPolar <59700990+Polarjd@users.noreply.github.com>
2023-03-12 15:49:20 +0100

    Comments and cleanup

--- main.py
+++ main.py
@@ -1,55 +1,76 @@
 import customtkinter
 from customtkinter import filedialog
 from customtkinter import *
-
 import replacefiles
 
+# Set appearance and color theme of the GUI
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")
+
+OPTIONS = {
+    "Replace Files": replacefiles.replace_files,
+    "Copy Files": replacefiles.copy_files
+}
 
 class ReplacioGUI:
     def __init__(self, master):
         self.master = master
         self.master.geometry("750x600")
         self.master.title("Replacio")
+
+        # Create StringVars to hold the paths of the source and destination folders
         self.source_folder_path = StringVar()
         self.destination_folder_path = StringVar()
+
+        # Call the create_widgets method to build the GUI
         self.create_widgets()
 
     def create_widgets(self):
+        # Create the first frame for the "Source Folder" section
         self.frame1 = customtkinter.CTkFrame(master=self.master)
-        self.frame1.pack(side="top",pady=20, padx=60, fill="x", expand=True)
+        self.frame1.pack(side="top", pady=20, padx=60, fill="x", expand=True)
 
+        # Create the label for the "Source Folder" section
         self.label1 = customtkinter.CTkLabel(master=self.frame1, text="Source Folder", font=("Arial", 30))
         self.label1.pack(pady=12, padx=10)
 
+        # Create the entry for the "Source Folder" section
         self.entry1 = customtkinter.CTkEntry(master=self.frame1, textvariable=self.source_folder_path)
         self.entry1.pack(pady=12, padx=100)
 
+        # Create the "Browse" button for the "Source Folder" section
         self.button1 = customtkinter.CTkButton(master=self.frame1, text="Browse", command=self.browse_source)
         self.button1.pack(pady=12, padx=10)
 
+        # Create the second frame for the "Destination Folder" section
         self.frame2 = customtkinter.CTkFrame(master=self.master)
-        self.frame2.pack(side="top",pady=20, padx=60, fill="x", expand=True)
+        self.frame2.pack(side="top", pady=20, padx=60, fill="x", expand=True)
 
+        # Create the label for the "Destination Folder" section
         self.label2 = customtkinter.CTkLabel(master=self.frame2, text="Destination Folder", font=("Arial", 30))
         self.label2.pack(pady=12, padx=10)
 
+        # Create the entry for the "Destination Folder" section
         self.entry2 = customtkinter.CTkEntry(master=self.frame2, textvariable=self.destination_folder_path)
         self.entry2.pack(pady=12, padx=10)
 
+        # Create the "Browse" button for the "Destination Folder" section
         self.button2 = customtkinter.CTkButton(master=self.frame2, text="Browse", command=self.browse_destination)
         self.button2.pack(pady=12, padx=10)
 
+        # Create the third frame for the "Replace or copy files" section
         self.frame3 = customtkinter.CTkFrame(master=self.master)
-        self.frame3.pack(side="top",pady=20, padx=60, fill="x", expand=True)
+        self.frame3.pack(side="top", pady=20, padx=60, fill="x", expand=True)
 
+        # Create the label for the "Replace or copy files" section
         self.label3 = customtkinter.CTkLabel(master=self.frame3, text="Replace or copy files from source to destination?", font=("Arial", 16))
         self.label3.pack(pady=12, padx=10)
 
+        # Create the OptionMenu for the "Replace or copy files" section
         self.optionmenu1 = customtkinter.CTkOptionMenu(master=self.frame3, values=["Replace Files", "Copy Files"])
         self.optionmenu1.pack(pady=12, padx=10)
 
+        # Create a button labeled "Start" in the frame3 window, with the command "run" assigned to it. 
         self.button3 = customtkinter.CTkButton(master=self.frame3, text="Start", command=self.run)
         self.button3.pack(pady=12, padx=10)
 
@@ -64,12 +85,14 @@ class ReplacioGUI:
         print(destination_filename)
 
     def run(self):
-        if self.optionmenu1.get() == "Replace Files":
-            replacefiles.replace_files(source_dir_var=self.source_folder_path, destination_dir_var=self.destination_folder_path)
-        elif self.optionmenu1.get() == "Copy Files":
-            replacefiles.copy_files(source_dir_var=self.source_folder_path, destination_dir_var=self.destination_folder_path)
-        print("Done!")
-        self.button3.configure(text="Done! Click again to start")
+        option = self.optionmenu1.get()
+        func = OPTIONS.get(option)
+        if func:
+            func(source_dir_var=self.source_folder_path, destination_dir_var=self.destination_folder_path)
+            print("Done!")
+            self.button3.configure(text="Done! Click again to start")
+        else:
+            print("Invalid option selected.")
 
 if __name__ == "__main__":
     root = customtkinter.CTk()
--- replacefiles.py
+++ replacefiles.py
@@ -2,24 +2,49 @@ import os
 import shutil
 
 def replace_files(source_dir_var, destination_dir_var):
+    # Copies files from source directory to destination directory, replacing any existing files with the same name and skipping any file that doesn't match an existing file.
+    
+    # Get the source and destination directories from the StringVars
     source_dir = source_dir_var.get()
     destination_dir = destination_dir_var.get()
+    
+    # Loop through the files in the destination directory
     for root, dirs, files in os.walk(destination_dir):
         for filename in files:
+            # Create paths to the source and destination files
             source_file = os.path.join(source_dir, filename)
             destination_file = os.path.join(root, filename)
-            if os.path.isfile(source_file):
-                shutil.copyfile(source_file, destination_file)
-                print(f"{filename} has been replaced in {destination_file}")
+            
+            # If the source file exists and is a file (not a directory), replace the source file with it
+            if os.path.isfile(source_file) and os.path.exists(destination_file):
+                try:
+                    shutil.copyfile(source_file, destination_file)
+                    print(f"{filename} has been replaced in {destination_file}")
+                except (FileNotFoundError, PermissionError) as e:
+                    print(f"Error occurred while replacing {filename}: {e}")
+            else:
+                print(f"{filename} not found in {source_dir} or {destination_file} not found in {destination_dir}")
 
 def copy_files(source_dir_var, destination_dir_var):
+    # Copies files from the source directory to each folder in the destination directory.
+    
+    # Get the source and destination directories from the StringVars
     source_dir = source_dir_var.get()
     destination_dir = destination_dir_var.get()
+    
+    # Loop through the folders in the destination directory
     for foldername in os.listdir(destination_dir):
+        # If the folder is actually a directory, copy files from the source directory to it
         if os.path.isdir(os.path.join(destination_dir, foldername)):
             destination = os.path.join(destination_dir, foldername)
             for filename in os.listdir(source_dir):
                 source = os.path.join(source_dir, filename)
-                if os.path.isfile(source):
-                    shutil.copy2(source, destination)
-                    print(f"{filename} has been copied to {destination}")
+                # If the file exists and is a file (not a directory), copy it to the destination folder
+                if os.path.isfile(source) and os.path.exists(destination):
+                    try:
+                        shutil.copy2(source, destination)
+                        print(f"{filename} has been copied to {destination}")
+                    except (FileNotFoundError, PermissionError) as e:
+                        print(f"Error occurred while copying {filename}: {e}")
+                else:
+                    print(f"{filename} not found in {source_dir} or {destination} not found in {destination_dir}")
